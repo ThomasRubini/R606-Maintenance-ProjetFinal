77ac4a66cc2fd3bfd40c36858b8b461db5fa1268
AudricV <74829229+AudricV@users.noreply.github.com>
2024-02-20 09:40:50 +0100

    Make data private field final in HashMapCatalog

--- src/main/java/dojo/supermarket/model/HashMapCatalog.java
+++ src/main/java/dojo/supermarket/model/HashMapCatalog.java
@@ -1,10 +1,11 @@
 package dojo.supermarket.model;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class HashMapCatalog implements SupermarketCatalog {
 
-    private HashMap<Product, Double> data = new HashMap<>();
+    private final Map<Product, Double> data = new HashMap<>();
 
     @Override
     public void addProduct(Product product, double price) {
