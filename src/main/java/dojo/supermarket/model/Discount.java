b3ea4e088f2911d25848a8f2eb1b965494deb5bc
AudricV <74829229+AudricV@users.noreply.github.com>
2024-02-20 10:11:28 +0100

    Add Javadoc for Discount class

--- src/main/java/dojo/supermarket/model/Discount.java
+++ src/main/java/dojo/supermarket/model/Discount.java
@@ -2,26 +2,56 @@ package dojo.supermarket.model;
 
 import java.util.Objects;
 
+/**
+ * Data class representing a discount.
+ *
+ * <p>
+ * A discount is composed of a description, a discount amount and a
+ * product on which the discount should be applied.
+ * </p>
+ */
 public class Discount {
 
     private final String description;
     private final double discountAmount;
     private final Product product;
 
+    /**
+     * Create a {@link Discount} instance.
+     *
+     * @param product        the product on which the discount should be applied
+     * @param description    the description of the discount
+     * @param discountAmount the amount of the discount
+     */
     public Discount(Product product, String description, double discountAmount) {
         this.product = product;
         this.description = description;
         this.discountAmount = discountAmount;
     }
 
+    /**
+     * Get the description of the discount.
+     *
+     * @return the description of the discount
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Get the amount of the discount.
+     *
+     * @return the amount of the discount
+     */
     public double getDiscountAmount() {
         return discountAmount;
     }
 
+    /**
+     * Get the product on which the discount should be applied.
+     *
+     * @return the product on which the discount should be applied
+     */
     public Product getProduct() {
         return product;
     }
