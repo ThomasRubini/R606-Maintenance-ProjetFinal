8c00daf4cc0d5908b4d96233b519bd39d238d464
AudricV <74829229+AudricV@users.noreply.github.com>
2024-02-20 09:56:02 +0100

    Add Javadoc for Product class

--- src/main/java/dojo/supermarket/model/Product.java
+++ src/main/java/dojo/supermarket/model/Product.java
@@ -2,20 +2,43 @@ package dojo.supermarket.model;
 
 import java.util.Objects;
 
+/**
+ * Data class for a product.
+ *
+ * <p>
+ * A product is composed of a name and a product unit.
+ * </p>
+ */
 public class Product {
 
     private final String name;
     private final ProductUnit unit;
 
+    /**
+     * Create a {@link Product} instance.
+     *
+     * @param name the name of the product
+     * @param unit {@link ProductUnit the unit} of the product
+     */
     public Product(String name, ProductUnit unit) {
         this.name = name;
         this.unit = unit;
     }
 
+    /**
+     * Get the name of the product.
+     *
+     * @return the name of the product
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the unit of the product.
+     *
+     * @return the unit of the product
+     */
     public ProductUnit getUnit() {
         return unit;
     }
