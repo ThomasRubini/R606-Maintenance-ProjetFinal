57e6c3825cd8935cb2384d994d144f4ab7640274
AudricV <74829229+AudricV@users.noreply.github.com>
2024-02-20 10:34:51 +0100

    Add Javadoc for Receipt class
    Also make getDiscounts return an unmodifiable list

--- src/main/java/dojo/supermarket/model/Receipt.java
+++ src/main/java/dojo/supermarket/model/Receipt.java
@@ -5,11 +5,34 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Class representing a receipt.
+ *
+ * <p>
+ * A receipt is composed of items and discounts which can be added one by one
+ * stored in separated lists.
+ * </p>
+ */
 public class Receipt {
 
     private final List<ReceiptItem> items = new ArrayList<>();
     private final List<Discount> discounts = new ArrayList<>();
 
+    /**
+     * Get the total price.
+     *
+     * <p>
+     * The total price is composed of the total price of all items on which the
+     * discount amount is subtracted.
+     * </p>
+     *
+     * <p>
+     * If there is no item and no discount, {@code 0.0} is returned; if there
+     * is no items and a discount, a negative double will be returned.
+     * </p>
+     *
+     * @return the total price, which can be lowed or equal to {@code 0.0}
+     */
     public double getTotalPrice() {
         double total = 0.0;
         for (ReceiptItem item : items) {
@@ -21,20 +44,48 @@ public class Receipt {
         return total;
     }
 
+    /**
+     * Add a product to the list of items.
+     *
+     * <p>
+     * A {@link ReceiptItem} will be built with the given parameters and added
+     * to the items list.
+     * </p>
+     *
+     * @param p          the product to add
+     * @param quantity   the quantity to use
+     * @param price      the price to use
+     * @param totalPrice the total price to use
+     */
     public void addProduct(Product p, double quantity, double price, double totalPrice) {
         items.add(new ReceiptItem(p, quantity, price, totalPrice));
     }
 
+    /**
+     * Get the list of items.
+     *
+     * @return the list of items, unmodifiable
+     */
     public List<ReceiptItem> getItems() {
         return Collections.unmodifiableList(items);
     }
 
+    /**
+     * Add a discount to the list of discounts.
+     *
+     * @param discount the discount to add
+     */
     public void addDiscount(Discount discount) {
         discounts.add(discount);
     }
 
+    /**
+     * Get the list of discounts.
+     *
+     * @return the list of discounts, unmodifiable
+     */
     public List<Discount> getDiscounts() {
-        return discounts;
+        return Collections.unmodifiableList(discounts);
     }
 
     @Override
